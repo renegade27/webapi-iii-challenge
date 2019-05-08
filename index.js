9f91d5ebca07a7d9dd4bfb8e5987775e28eba243
renegade27 <foemaster@live.com>
2019-05-08 17:43:23 -0500

    Custom middleware implemented. No client developed yet for use with API. MVP Completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code away!
+const express = require('express');
+const middleware = require('./middlewares');
+
+const server = express();
+
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+
+server.use(express.json());
+server.use(middleware.capital);
+server.use('/api', postRoutes);
+server.use('/api', userRoutes);
+
+server.listen(5000, () => {
+    console.log("Server is listening on port 5000.")
+})
